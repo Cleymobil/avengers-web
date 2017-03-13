b9f3f59a64db3d85dc09d6e1ee5ed98b274f4e36
CBaty <camille.defroberville@gmail.com>
2017-03-13 16:33:24 +0100

    MovieResource.java

---
+++ src/io/avengers/ws/MovieResource.java
@@ -0,0 +1,34 @@
+package io.avengers.ws;
+
+import java.util.Set;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import io.avengers.domain.Movie;
+import io.avengers.service.MovieService;
+import javax.ws.rs.core.MediaType;
+
+
+
+
+@Path("movies")
+@Produces(MediaType.APPLICATION_JSON)
+public class MovieResource {
+	@GET
+	public Set<Movie> getAllMovies(){
+		MovieService mService = new MovieService();
+		return mService.findAll();
+		
+	}
+	
+	@GET
+	public Set<Movie> getMoviesByName(@PathParam("term") String term){
+		MovieService mService = new MovieService();
+		return mService.findMoviesByName(term);
+		
+	}
+
+}
