cc24f175fbb2458cebe2006a27c0ea46442f28e4
saramencor <sara.mendez.cortinas@gmail.com>
2017-03-22 11:56:23 +0100

    webpack and bundles

--- WebContent/js/heroe.js
+++ WebContent/js/heroe.js
@@ -1,130 +1,16 @@
-function HeroListComponent() {
-    $('button.create').on('click', event => this.createHero())
+import { HeroListComponent } from "./heroes/heroListComponent";
 
+
+let component;
+
+function heroApplication() {
+    // now jQuery is ready
+    component = new HeroListComponent();
+    component.fetchAll().then(function(heroes) {
+        component.render(heroes);
+    });
 }
-HeroListComponent.prototype = {
-    fetchAll: function() {
-        return $.get('marvel/heroes')
-            .then(json => {
-                this.collection = [];
-                json.forEach(data => {
-                    const hero = new HeroItem(data, this);
-                    this.collection.push(hero);
-                });
-                return this.collection;
-            });
-    },
-    render: function() {
-        const template = `<div class = component>
-			<h1>Hero List</h1> 
-				<ul>
-				</ul>
-				<footer> Some html footer</footer>
-			</div>`;
-        //cached component jQueryified element
-        this.$el = $(template);
-        console.log(this.$el);
-        //All is done in Memory
-        this.collection.forEach(hero => this.$el.find('ul').append(hero.render()));
-        //More efficient, if we put in the DOM later
-        $('div.heroList').append(this.$el);
-        return this.$el;
-    },
-    createHero() {
-
-        const newHero = {
-            name: $('input[name=name]').val(),
-            likes: parseInt($('input[name=likes]').val()),
-            dislikes: parseInt($('input[name=dislikes]').val()),
-            team: $('input[name=team]').val()
-        }
-        const newHeroItem = new HeroItem(newHero, this);
-        const me = this;
-        fetch('marvel/heroes', {
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                method: "POST",
-                body: JSON.stringify(newHero)
-            })
-            .then(response => {
-                response.json().then(json => {
-                    console.log(json);
-                    const character = new HeroItem(json, me);
-                    me.add(character);
-                })
-            });
-    },
-    add: function(newHero) {
-        console.log(newHero.id);
-        this.collection.push(newHero);
-        this.$el.find('ul').append(newHero.render());
-        console.log(this.$el);
-        return this.collection;
-    },
-    renderCheckedList: function() {
-        const template = `<div class = component>
-			<h3>Hero to add in the movie</h3> 
-				<ul>
-				</ul>
-			</div>`;
-        //cached component jQueryified element
-        this.$el = $(template);
-        console.log(this.$el);
-        //All is done in Memory
-        this.collection.forEach(hero => this.$el.find('ul').append(hero.renderCheck()));
-        //More efficient, if we put in the DOM later
-        $('#checkedHeroList').append(this.$el);
-        return this.$el;
-    },
-}
-
-function HeroItem(data, listComponent) {
-    Object.assign(this, data);
-    this.listComponent = listComponent;
-    this.collection = listComponent.collection;
-}
-
-HeroItem.prototype = {
-    render() {
-
-        const template = `<li>${this.name}&nbsp&nbsp<button class="delete">Delete ${this.name}</button>&nbsp&nbsp&nbsp&nbsp<button class="view">View ${this.name}</button></li>`;
-        //Element jQueryfied
-        this.$el = $(template);
-        const li = $('<li>');
-        //Catch the button without reading all DOM with find()
-        const button = this.$el.find('button.delete').on('click', event => this.remove());
-        const button2 = this.$el.find('button.view').on('click', event => this.viewHero());
-
-        return this.$el;
-    },
-    viewHero: function() {
-        return $.get('marvel/heroes/' + this.id)
-        .then(Response => {
-            $('div.component').remove();
-            const template = `<div class="component"><h1>${this.name}</h1> <ul><li>Name:${this.name}</li><li>Id:${this.id}</li><li>Likes:${this.likes}</li></ul><h1></h1></div>`;
-            this.$el = $(template);
-            $('body').append(this.$el);
-            //this.viewHeroes();
-
-        })
-    },
-
-    remove() {
-        fetch('marvel/heroes/' + this.id, {
-                method: 'delete'
-            })
-            .catch(error => heroApplication());
-        //new state
-        component.collection = component.collection.filter(h => h.id !== this.id);
-        this.$el.remove();
-    },
-    renderCheck() {
-        const template = `<li><input class="checkboxHero" type= "checkbox" name = ${this.id}>${this.name}</li>`;
-        //Element jQueryfied
-        this.$el = $(template);
-        const li = $('<li>');
-        return this.$el;
-    }
-}
+// after 25ms
+$(document).ready(function() {
+    heroApplication();
+});
--- WebContent/js/movies.js
+++ WebContent/js/movies.js
@@ -1,142 +1,21 @@
-function MovieListComponent() {
-    $('button.createMovie').on('click', event => this.createMovie());
-    $('button.createMovie2').on('click', event => this.createMovie2());
+import { MovieListComponent } from "./movies/movieListComponent";
+import { HeroListComponent } from "./heroes/heroListComponent";
+
+let component;
+
+function movieApplication() {
+    // now jQuery is ready
+    component = new MovieListComponent();
+    component.fetchAll().then(function(movies) {
+        component.render(movies);
+        component.renderList(movies);
+    })
+    let heroListComponent = new HeroListComponent();
+    heroListComponent.fetchAll().then(function(heroes) {
+        heroListComponent.renderCheckedList(heroes);
+    })
 }
-MovieListComponent.prototype = {
-    fetchAll: function() {
-        return $.get('marvel/movies')
-            .then(json => {
-                this.collection = [];
-                json.forEach(data => {
-                    const movie = new MovieItem(data, this);
-                    this.collection.push(movie);
-                });
-                return this.collection;
-            });
-    },
-    render: function() {
-        const template = `<div class = component>
-			<h1>Movie List</h1> 
-				<ul>
-				</ul>
-				<footer> Some html footer</footer>
-			</div>`;
-        //cached component jQueryified element
-        this.$el = $(template);
-        console.log(this.$el);
-        //All is done in Memory
-        this.collection.forEach(movie => this.$el.find('ul').append(movie.render()));
-        //More efficient, if we put in the DOM later
-        $('div.movieList').append(this.$el);
-        return this.$el;
-    },
-    renderList: function() {
-        const template = `<p><select name="movieSelectName" id="movieSelect">
-       </select><p/>`;
-        //cached component jQueryified element
-        this.$el = $(template);
-        console.log(this.$el);
-        //All is done in Memory
-        this.collection.forEach(movie => this.$el.find('#movieSelect').append(movie.renderInList()));
-        //More efficient, if we put in the DOM later
-        $('#movieSelectId').append(this.$el);
-        return this.$el;
-    },
-    createMovie() {
-
-        let movie = {
-            name: $('input[name=movieName]').val(),
-            gross: parseInt($('input[name=gross]').val()),
-            budget: parseInt($('input[name=budget]').val()),
-        }
-        let newMovieItem = new MovieItem(movie, this);
-        console.log(movie);
-        const me = this;
-        fetch('marvel/movies', {
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                method: "POST",
-                body: JSON.stringify(movie)
-            })
-            .then(response => {
-                response.json().then(json => {
-                    console.log(json);
-                    const newMovie = new MovieItem(json, me);
-                    me.add(newMovie);
-                })
-            });
-    },
-    add: function(movie) {
-        console.log(movie.id);
-        this.collection.push(movie);
-        this.$el.find('ul').append(movie.render());
-        console.log(this.$el);
-        return this.collection;
-    },
-    createMovie2() {
-
-        let movie = {
-            name: $('input[name=movieName2]').val(),
-            gross: parseInt($('input[name=gross2]').val()),
-            budget: parseInt($('input[name=budget2]').val()),
-            history: $('input[name=history2]').val()
-        }
-        let newMovieItem = new MovieItem(movie, this);
-        console.log(movie);
-        fetch('marvel/movies/', {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            method: "POST",
-            body: JSON.stringify(movie)
-        }).then(json => {
-            this.collection.push(newMovieItem);
-            this.$el.find('ul').append(newMovieItem.render())
-        });
-    }
-}
-
-
-function MovieItem(data, listComponent) {
-    Object.assign(this, data);
-    this.listComponent = listComponent;
-    this.collection = listComponent.collection;
-}
-
-MovieItem.prototype = {
-    render() {
-
-        const template = `<li>${this.name}<button>Delete ${this.name}</button></li>`;
-        //Element jQueryfied
-        this.$el = $(template);
-        const li = $('<li>');
-        //Catch the button without reading all DOM with find()
-        const button = this.$el.find('button').on('click', event => this.remove());
-        return this.$el;
-    },
-    renderInList() {
-        const template = `<option value="${this.name}">${this.name}</option>`;
-        //Element jQueryfied
-        this.$el = $(template);
-        return this.$el;
-    },
-
-    remove() {
-        fetch('marvel/movies/' + this.id, { method: 'delete' })
-            .catch(error => movieApplication());
-        //new state
-        component.collection = component.collection.filter(h => h.id !== this.id);
-        this.$el.remove();
-    },
-    addHeroInMovie() {
-        const heroes_id = [];
-        $('input.check:checked').each(function() {
-            heroes_id.push($(this).name());
-        });
-        const movie_name = $("#movieSelect").val().id;
-
-    }
-}
+// after 25ms
+$(window).on("load", function() {
+    movieApplication();
+});
